781c1b020ee7154a4dc92566bc147295ed238209
MyName <myname@gmail.com>
2023-10-05 18:11:19 -0700

    ddd

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,33 +1,29 @@
 # instal and configure nginx
-if ! which('nginx') {
-  exec { 'update':
-    command     => '/usr/bin/apt-get update',
-  }
-  -> package { 'nginx':
-    ensure => installed,
-  }
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
--> exec { 'run':
+-> package { 'nginx':
+  ensure => installed,
+}
+-> exec { 'run1':
   command => '/usr/bin/mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"',
 }
--> exec { 'run':
+-> exec { 'run2':
   command => '/usr/bin/echo "Hi!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
 }
--> exec { 'run':
+-> exec { 'run3':
   command => '/usr/bin/rm -rf /data/web_static/current',
 }
--> exec { 'run':
+-> exec { 'run4':
   command => '/usr/bin/ln -s /data/web_static/releases/test/ /data/web_static/current',
 }
--> exec { 'run':
+-> exec { 'run5':
   command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
 }
--> file_line { 'location_hbnb_static':
-  path     => '/etc/nginx/sites-available/default',
-  match    => '^server {',
-  line     => 'location /hbnb_static {alias /data/web_static/current/;index index.html;}',
-  multiple => false,
+-> exec { 'hbnb_static':
+  command => 'sudo sed -i "/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}" /etc/nginx/sites-enabled/default',
+  provider => shell,
 }
--> exec { 'run':
+-> exec { 'run6':
   command => '/usr/sbin/service nginx restart',
 }
